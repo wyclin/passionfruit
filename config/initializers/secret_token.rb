8656dbf2db9e2c4d191446a71a3c4f21097084bb
Wayne Lin <waynelin.tw@gmail.com>
2012-07-04 18:25:58 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inaika::Application.config.secret_token = '96a374ba92627cccd6f51ba19beba7514564a7e97bb31d1082eb3e0f31f0c40dc954a49707bfa49ffab1bb0095d45a70bdaed8e55139b3faa17bd26133e4fd36'
