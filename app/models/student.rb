c065cf3c6543314ad4e69f64c051afad2b0773a8
Wayne Lin <waynelin.tw@gmail.com>
2012-07-18 01:37:55 +0200

    Re-add student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,7 +5,7 @@ class Student < ActiveRecord::Base
 
   validates :first_name, :last_name, presence: true
   validates :password, presence: true, length: {minimum: 6}
-  validates :password_confirmation, presence: true
+  validates :password_confirmation,  presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
