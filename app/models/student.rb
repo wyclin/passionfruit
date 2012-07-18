360b9e821a51789274681b5a7fe4f114cc4298de
Wayne Lin <waynelin.tw@gmail.com>
2012-07-18 02:02:14 +0200

    Add student.rb

---
+++ app/models/student.rb
@@ -0,0 +1,12 @@
+class Student < ActiveRecord::Base
+  attr_accessible :email, :first_name, :image, :last_name, :location
+
+  before_save { |student| student.email = email.downcase }
+
+  validates :first_name, :last_name, presence: true
+  validates :password, presence: true, length: {minimum: 6}
+  validates :password_confirmation,  presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+end
